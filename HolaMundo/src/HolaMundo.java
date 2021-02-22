149c64684daca87b24300ffbac16be79b01b4891
Juanvi Costa <costa@ieselcaminas.org>
2021-02-22 10:40:18 +0100

    Commit de refactorización

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("HolaMundo");
 		System.out.println("PRIMER COMMIT");
+		System.out.println("REFACTORIZAMOS EL CÓDIGO");
 	}
 }
