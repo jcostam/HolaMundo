d5f879333869819efc1a1da0f19f57167fba1b29
Juanvi Costa <costa@ieselcaminas.org>
2021-02-22 10:50:20 +0100

    Commit para subir el código documentado

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,5 +1,16 @@
-
+/**
+ * Clase de ejemplo para mostrar cómo se crean ramas en Eclipse y se suben 
+ * a GitHub
+ * @author Profesor
+ * @version 1.1.0.
+ * @since 22/02/2021
+ *
+ */
 public class HolaMundo {
+	/**
+	 * Programa principal
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("HolaMundo");
 		System.out.println("PRIMER COMMIT");
